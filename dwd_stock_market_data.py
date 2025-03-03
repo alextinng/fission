d03eeb491130cbb4e6a39e798fbf6e915f26b332
dingaolong <dingaolong@sungrowpower.com>
2025-03-03 19:37:03 +0800

    新增行情处理处理代码

---
+++ dwd_stock_market_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import duckdb
+
+df = pd.read_csv("D:\\Private\\fission\\stock_market_data.text")
+df["下一天开盘"] = df["开盘"].shift(-1)
+df["下一天收盘"] = df["收盘"].shift(-1)
+df["下一天最高"] = df["最高"].shift(-1)
+df["下一天最低"] = df["最低"].shift(-1)
+df["下一天成交量"] = df["成交量"].shift(-1)
+
+df = df.sort_values(by=['股票代码','日期'])
+df['涨跌幅'] = df['收盘'].pct_change()
+df["下一天涨跌幅"] = df["涨跌幅"].shift(-1)
+
+df['下一天最高涨幅'] = (df['下一天最高'] - df['收盘']) * 100 / df['收盘']
+df['下一天最低跌幅'] = (df['下一天最低'] - df['收盘']) * 100 / df['收盘']
+df['下一天成交量变化幅度'] = (df['下一天成交量'] - df['成交量']) * 100 / df['成交量']
+
+df.to_csv("D:\\Private\\fission\\dwd_stock_market_data.text", index=False, header=True, sep=",", encoding="utf-8")
