fe57fbea195ef8b74bdd602e9019ddedffcaa73c
Alex Ting <alextinng@hotmail.com>
2025-03-05 08:57:12 +0800

    新增成交量变化幅度字段

--- dwd_stock_market_data.py
+++ dwd_stock_market_data.py
@@ -15,5 +15,6 @@ df['涨跌幅'] = df['下一天涨跌幅'].shift(1)
 df['下一天最大涨跌幅'] = (df['下一天最高'] - df['收盘']) * 100 / df['收盘']
 df['下一天最小涨跌幅'] = (df['下一天最低'] - df['收盘']) * 100 / df['收盘']
 df['下一天成交量变化幅度'] = (df['下一天成交量'] - df['成交量']) * 100 / df['成交量']
+df['成交量变化幅度'] = df['下一天成交量变化幅度'].shift(1)
 
 df.to_csv("D:\\Private\\fission\\dwd_stock_market_data.text", index=False, header=True, sep=",", encoding="utf-8")
